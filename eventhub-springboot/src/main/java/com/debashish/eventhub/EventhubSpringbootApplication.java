8bdd5dc7b007e9280ab00df0b6a42041f896a67a
Barik <debashish.barik@accenture.com>
2020-01-05 14:59:04 +0530

    inital version

---
+++ eventhub-springboot/src/main/java/com/debashish/eventhub/EventhubSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.debashish.eventhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhubSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhubSpringbootApplication.class, args);
+	}
+
+}
---
+++ eventhub-springboot/src/test/java/com/debashish/eventhub/EventhubSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.debashish.eventhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventhubSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
